add378eb024fe54312f72b1f01e5149932059f03
Dreadpipe <benharris83@gmail.com>
2019-02-17 20:04:55 -0600

    Bootstrap added, because I guess I'm not cool enough for mobile responsive yet

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+// This is a grandparent function that holds my jQuery/javascript
+// I need this so the document(html) properly loads before executing my programs
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is closing parenthetical for my document.ready function
+});
